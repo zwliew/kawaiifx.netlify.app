a1edb81a484b9c43b13d1344527da5ce3cc9db34
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-01-19 11:27:32 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Mayuri = function(window, el) {
+  const player = el.querySelector('#player')
+
+  function start() {
+    player.play()
+    el.classList.add('animated')
+    el.classList.add('tada')
+    window.setTimeout(stop, 1000)
+  }
+
+  function stop() {
+    el.classList.remove('animated')
+    el.classList.remove('tada')
+  }
+
+  el.addEventListener('click', start)
+}
+
+window.addEventListener('load', () => {
+  const mayuri = new Mayuri(window, document.getElementById('mayuri'))
+})
