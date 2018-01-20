2c15449b4602cd7fdc3aa36a18194b7dea096d1c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-01-20 21:47:01 +0800

    Use Web Components for the character

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,15 @@
-const Character = function(el) {
-  const player = el.querySelector('#player')
-
-  function start() {
-    player.play()
-    el.classList.add('animated')
-    el.classList.add('tada')
-    setTimeout(stop, 1000)
-  }
-
-  function stop() {
-    el.classList.remove('animated')
-    el.classList.remove('tada')
-  }
-
-  el.addEventListener('click', start)
-}
-
 window.addEventListener('load', () => {
-  const character = new Character(document.getElementById('character'))
+  const character = document.getElementById('character')
+  const container = document.getElementById('char-container')
+  character.addEventListener('click', () => {
+    character.play()
+    container.classList.add('animated')
+    container.classList.add('tada')
+    setTimeout(() => {
+      container.classList.remove('animated')
+      container.classList.remove('tada')
+    }, 1000)
+  })
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
